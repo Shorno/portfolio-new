4f5d056c0d0d100621e2ef5c01d0ec1247a5122b
Shorno <fb.shorno@gmail.com>
2026-06-04 14:31:34 +0600

    update favicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -10,7 +10,19 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#0B0B0C",
-    theme_color: "#0B0B0C",
+    theme_color: "#EA7948",
+    icons: [
+      {
+        src: "/icon.svg",
+        sizes: "any",
+        type: "image/svg+xml",
+      },
+      {
+        src: "/apple-icon.svg",
+        sizes: "180x180",
+        type: "image/svg+xml",
+      },
+    ],
     lang: "en",
   };
 }
